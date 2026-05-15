83fa74129ccdd367d5d32204d6049311bc57de49
facebookkhongten456789-del <your-email@example.com>
2026-05-15 19:46:13 +0700

    deploy: update and deploy to github

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,23 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' https://res.cloudinary.com https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline' https://challenges.cloudflare.com; img-src 'self' data: https: https://res.cloudinary.com; font-src 'self' data:; connect-src 'self' https: https://challenges.cloudflare.com; frame-src 'self' https://challenges.cloudflare.com; upgrade-insecure-requests;",
+            value: "default-src 'self'; script-src 'self' https://res.cloudinary.com https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline' https://challenges.cloudflare.com; img-src 'self' data: https: https://res.cloudinary.com; font-src 'self' data:; connect-src 'self' https: https://challenges.cloudflare.com; frame-src 'self' https://challenges.cloudflare.com; upgrade-insecure-requests;",
+          },
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
           },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://tech-portal-gilt.vercel.app", // Thay bằng domain chính xác của bạn
+            value: "https://tech-portal-gilt.vercel.app",
           },
           {
             key: "X-Frame-Options",
